b650b3c3107cfc5f5e36a39575e4e7300ceb12b7
kiva <kiva515@foxmail.com>
2017-08-28 14:46:47 +0800

    gen-files: fix time string

--- gen-inrelease.sh
+++ gen-inrelease.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+export LC_TIME=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_COLLATE=en_US.UTF-8
+
 cat <<EOF
 Codename: stable
 Version: 1
-Architectures: all aarch64
+Architectures: all aarch64 arm x86_64
 Description: Main repository
 Suite: stable
 Date: $(date -u "+%a, %d %b %Y %H:%m:%S UTC")
